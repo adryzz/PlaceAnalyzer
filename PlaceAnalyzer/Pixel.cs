0b80f930808bf45d6ba0f976322f7d4483489a03
Adryzz <adryzz@vignetteapp.org>
2022-04-08 18:41:25 +0200

    Improve speed and stuff

--- PlaceAnalyzer/Pixel.cs
+++ PlaceAnalyzer/Pixel.cs
@@ -1,5 +1,4 @@
 using System.Drawing;
-using System.Globalization;
 using NodaTime;
 using NodaTime.Text;
 
@@ -7,47 +6,178 @@ namespace PlaceAnalyzer;
 
 public struct Pixel
 {
+    public static Dictionary<Uid, uint> UserMap = new Dictionary<Uid, uint>();
+
+    public static uint Users = 0;
+
     public static readonly InstantPattern TimestampPattern =
         InstantPattern.CreateWithInvariantCulture("yyyy-MM-dd HH:mm:ss.fff 'UTC'");
-    public Instant Timestamp { get; set; }
-    
-    public string Uid { get; set; }
-    
-    public Color Color { get; set; }
-    
-    public ushort X { get; set; }
-    
-    public ushort Y { get; set; }
-    
-    /// <summary>
-    /// Mod only
-    /// </summary>
-    public ushort Width { get; set; }
-    
-    /// <summary>
-    /// Mod only
-    /// </summary>
-    public ushort Height { get; set; }
 
+    //public Instant Timestamp;
+
+    public uint Uid;
+
+    private uint internalPosColor; // 11 bits X, 11 bits Y, 5 bits color
+
+    public uint Color => internalPosColor << 27;
+
+    public uint X => internalPosColor << 11;
+
+    public uint Y => internalPosColor << 22;
+    /*
+    /// <summary>
+    /// Mod only
+    /// </summary>
+    public ushort Width;
+    
+    /// <summary>
+    /// Mod only
+    /// </summary>
+    public ushort Height;*/
     public Pixel(RedditPixel p)
     {
-        Uid = p.Uid;
-        Color = ColorTranslator.FromHtml(p.Color);
-        
-        Timestamp = TimestampPattern.Parse(p.Timestamp).Value;
+        Uid u = new Uid(p.Uid);
+        lock (UserMap)
+        {
+            if (UserMap.ContainsKey(u))
+            {
+                Uid = UserMap[u];
+            }
+            else
+            {
+                uint i = Interlocked.Increment(ref Users);
 
+                UserMap.Add(u, i);
+                Uid = i;
+            }
+        }
+
+        var c = ColorTranslator.FromHtml(p.Color);
+        Colors color = (Colors)(((c.A << 24) | (c.R << 16) | (c.G << 8) | c.B) & 0xffffffffL);
+        
+        //Timestamp = TimestampPattern.Parse(p.Timestamp).Value;
+        //Timestamp = default;
         string[] pos = p.Coordinates.Split(',');
         
-        X = ushort.Parse(pos[0]);
-        Y = ushort.Parse(pos[1]);
-        
-        Width = 0;
+        internalPosColor = uint.Parse(pos[0]);
+        internalPosColor |= uint.Parse(pos[1]) << 11;
+        internalPosColor |= (uint) color << 27;
+
+        /*Width = 0;
         Height = 0;
         
         if (pos.Length > 2)
         {
             Width = ushort.Parse(pos[2]);
             Width = ushort.Parse(pos[3]);
-        }
+        }*/
     }
+    
+    public enum Colors : uint
+    {
+        Burgundy = 0x6D001A,
+        DarkRed = 0xBE0039,
+        Red = 0xFF4500,
+        Orange = 0xFFA800,
+        Yellow = 0xFFD635,
+        PaleYellow = 0xFFF8B8,
+        DarkGreen = 0x00A368,
+        Green = 0x00CC78,
+        LightGreen = 0x7EED56,
+        DarkTeal = 0x00756F,
+        Teal = 0x009EAA,
+        LightTeal = 0x00CCC0,
+        DarkBlue = 0x2450A4,
+        Blue = 0x3690EA,
+        LightBlue = 0x51E9F4,
+        Indigo = 0x493AC1,
+        Periwinkle = 0x6A5CFF,
+        Lavender = 0x94B3FF,
+        DarkPurple = 0x811E9F,
+        Purple = 0xB44AC0,
+        PalePurple = 0xE4ABFF,
+        Magenta = 0xDE107F,
+        Pink = 0xFF3881,
+        LightPink = 0xFF99AA,
+        DarkBrown = 0x6D482F,
+        Brown = 0x9C6926,
+        Beige = 0xFFB470,
+        Black = 0x000000,
+        DarkGray = 0x515252,
+        Gray = 0x898D90,
+        LightGray = 0xD4D7D9,
+        White = 0xFFFFFF
+    }
+    
+    public static byte GetColor(Colors color) => color switch
+    {
+        Colors.Burgundy => 0,
+        Colors.DarkRed => 1,
+        Colors.Red => 2,
+        Colors.Orange => 3,
+        Colors.Yellow => 4,
+        Colors.PaleYellow => 5,
+        Colors.DarkGreen => 6,
+        Colors.Green => 7,
+        Colors.LightGreen => 8,
+        Colors.DarkTeal => 9,
+        Colors.Teal => 10,
+        Colors.LightTeal => 11,
+        Colors.DarkBlue => 12,
+        Colors.Blue => 13,
+        Colors.LightBlue => 14,
+        Colors.Indigo => 15,
+        Colors.Periwinkle => 15,
+        Colors.Lavender => 16,
+        Colors.DarkPurple => 17,
+        Colors.Purple => 18,
+        Colors.PalePurple => 19,
+        Colors.Magenta => 20,
+        Colors.Pink => 21,
+        Colors.LightPink => 23,
+        Colors.DarkBrown => 24,
+        Colors.Brown => 25,
+        Colors.Beige => 26,
+        Colors.Black => 27,
+        Colors.DarkGray => 28,
+        Colors.Gray => 29,
+        Colors.LightGray => 30,
+        Colors.White => 31
+    };
+    
+    public static Colors GetColor(byte color) => color switch
+    {
+        0 => Colors.Burgundy,
+        1 => Colors.DarkRed,
+        2 => Colors.Red,
+        3 => Colors.Orange,
+        4 => Colors.Yellow,
+        5 => Colors.PaleYellow,
+        6 => Colors.DarkGreen,
+        7 => Colors.Green,
+        8 => Colors.LightGreen,
+        9 => Colors.DarkTeal,
+        10 => Colors.Teal,
+        11 => Colors.LightTeal,
+        12 => Colors.DarkBlue,
+        13 => Colors.Blue,
+        14 => Colors.LightBlue,
+        15 => Colors.Indigo,
+        16 => Colors.Periwinkle,
+        17 => Colors.Lavender,
+        18 => Colors.DarkPurple,
+        19 => Colors.Purple,
+        20 => Colors.PalePurple,
+        21 => Colors.Magenta,
+        22 => Colors.Pink,
+        23 => Colors.LightPink,
+        24 => Colors.DarkBrown,
+        25 => Colors.Brown,
+        26 => Colors.Beige,
+        27 => Colors.Black,
+        28 => Colors.DarkGray,
+        29 => Colors.Gray,
+        30 => Colors.LightGray,
+        31 => Colors.White
+    };
 }
--- PlaceAnalyzer/Program.cs
+++ PlaceAnalyzer/Program.cs
@@ -1,25 +1,32 @@
 ﻿using System.Diagnostics;
+using System.Drawing;
 using System.Globalization;
 using System.IO.Compression;
 using System.Text;
+using System.Text.Json;
 using CsvHelper;
+using StreamUtils;
 
 namespace PlaceAnalyzer
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static unsafe void Main(string[] args)
         {
-            IEnumerable<string> files = Directory.EnumerateFiles("/mnt/adryzz/stats", "*.gzip");
-            foreach (var file in files)
+            Console.WriteLine(sizeof(Pixel));
+            int count = 0;
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            List<Pixel> pixels = new List<Pixel>(12000000);
+
+            IEnumerable<string> files = Directory.EnumerateFiles("/run/media/adryzz/a", "*.gzip");
+            foreach(string file in files)
             {
-                int count = 0;
-                Stopwatch stopwatch = Stopwatch.StartNew();
-                
-                FileStream fs = File.OpenRead(file);
-                BufferedStream buf = new BufferedStream(fs);
-                GZipStream gzip = new GZipStream(buf, CompressionMode.Decompress);
-                
+                Console.WriteLine($"Loading {file}...");
+                using FileStream fs = File.OpenRead(file);
+                using BufferedStream buf = new BufferedStream(fs);
+                using GZipStream gzip = new GZipStream(buf, CompressionMode.Decompress);
+
                 using (var reader = new StreamReader(gzip))
                 using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
                 {
@@ -28,11 +35,71 @@ namespace PlaceAnalyzer
                     foreach (var rpix in records)
                     {
                         Pixel pix = new Pixel(rpix);
-                        count++;
+                        pixels.Add(pix);
                     }
                 }
-                Console.WriteLine(stopwatch.Elapsed);
+                gzip.Dispose();
+                buf.Dispose();
+                fs.Dispose();
+                Console.WriteLine($"File loaded! pixels: {pixels.Count}");
+                Console.WriteLine($"Current memory usage: {format(GC.GetTotalMemory(true))}");
+                Interlocked.Increment(ref count);
+                Console.WriteLine($"loaded {count}/{files.Count()} files");
             }
+        }
+        
+
+        static bool inRange(Pixel p, int x, int y, int w, int h)
+        {
+            return (p.X >= x && p.Y >= y && p.X <= w && p.Y <= h);
+        }
+
+        static string format(long i)
+        {
+
+                // Get absolute value
+                long absolute_i = (i < 0 ? -i : i);
+                // Determine the suffix and readable value
+                string suffix;
+                double readable;
+                if (absolute_i >= 0x1000000000000000) // Exabyte
+                {
+                    suffix = "EB";
+                    readable = (i >> 50);
+                }
+                else if (absolute_i >= 0x4000000000000) // Petabyte
+                {
+                    suffix = "PB";
+                    readable = (i >> 40);
+                }
+                else if (absolute_i >= 0x10000000000) // Terabyte
+                {
+                    suffix = "TB";
+                    readable = (i >> 30);
+                }
+                else if (absolute_i >= 0x40000000) // Gigabyte
+                {
+                    suffix = "GB";
+                    readable = (i >> 20);
+                }
+                else if (absolute_i >= 0x100000) // Megabyte
+                {
+                    suffix = "MB";
+                    readable = (i >> 10);
+                }
+                else if (absolute_i >= 0x400) // Kilobyte
+                {
+                    suffix = "KB";
+                    readable = i;
+                }
+                else
+                {
+                    return i.ToString("0 B"); // Byte
+                }
+                // Divide by 1024 to get fractional value
+                readable = (readable / 1024);
+                // Return formatted number with suffix
+                return readable.ToString("0.### ") + suffix;
         }
     }
 }
